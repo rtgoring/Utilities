10a148ac7e1a96d096b5fc69ae780d87b4586166
rtgoring <rtgoring@gmail.com>
2017-08-13 13:46:08 -0400

    Initial Publish

---
+++ createAnnotationFiles.py
@@ -0,0 +1,22 @@
+import os
+
+
+def main():
+    """
+    Crawls through all folders in directory and creates
+    the Annotations folder if not already present
+    :return:
+    """
+    directory = '/home/goring/Documents/DataSets/Sub/2017/Forward'
+    for a in os.listdir(directory):
+        try:
+            os.mkdir(os.path.join(directory, a, "Annotations"))
+        except:
+            pass
+            # print 'Folder Already Made'
+
+    return True
+
+
+if __name__ == '__main__':
+    main()
