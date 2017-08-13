9eb102f9a452720732215d612012549f5ff7d7f2
rtgoring <rtgoring@gmail.com>
2017-08-13 17:38:37 -0400

    Initial Publish

--- annotationInfo.py
+++ annotationInfo.py
@@ -1,100 +1,115 @@
+import csv
 import os
 import xml.etree.ElementTree as ET
-import csv
 
 
 def parse_annotation(path, file):
     """
     Function to parse XML files and return dict
-      :return:
+      :return: Dictionary containing content counters of XMLFile
     """
 
-    localDict = {}
+    localdict = {}
     in_file = open(path + '/' + file)
     tree = ET.parse(in_file)
     root = tree.getroot()
 
-    localDict['filePath'] = path
-    localDict['fileName'] = file
+    localdict['filePath'] = path
+    localdict['fileName'] = file
 
     for obj in root.iter('object'):
         cls = obj.find('name').text
+
+        # Add each class to allClasses if not present
         if cls not in allClasses:
             allClasses.append(cls)
 
-        if cls in localDict:
-            localDict[cls] = localDict[cls] + 1
+        # Count number of times that class is in XML file
+        if cls in localdict:
+            localdict[cls] = localdict[cls] + 1
         else:
-            localDict[cls] = 1
-    return localDict
+            localdict[cls] = 1
+    return localdict
 
 
 if __name__ == "__main__":
+    # ###############
+    # Editable Params
+    # ###############
+
     directory = '/home/goring/Documents/DataSets/Sub/2017/Forward'
-    CSVFILENAME = directory.split('/')[-1]
-    allClasses = []
-    allImages = 0
+    CSVFILENAME = directory.split('/')[-1]  # last folder in directory path, could be anything
+
+    allClasses = []  # Global to keep track of all annotated classes
+    dictArray = []  # Global to keep all returned dicts - Contains entry for each XML file
+    folderDicts = []  # Reduces dict array to one dict per folder
+
+    allImages = 0  # Global to keep track of number of annotated image - One XML file for each image
+
     # Get listing of all annotation directories
     if os.path.isdir(directory):
-        dirToAnnots = []
+        dirToAnnots = []  # Directories that contain annotations
         for a in os.listdir(directory):
             annotationDir = os.path.join(directory, a, 'Annotations')
-            if os.path.isdir(annotationDir):
+            if os.path.isdir(annotationDir):  # Ensures folder exits
                 dirToAnnots.append(annotationDir)
-    dictArray = []
-    for b in dirToAnnots:
-        for c in os.listdir(b):
+
+    # Creates Dict for each annotation
+    for b in dirToAnnots:  # For each folder
+        for c in os.listdir(b):  # For each annotation
             retDict = parse_annotation(b, c)
             dictArray.append(retDict)
 
-    folderDicts = []
-    for dTA in dirToAnnots:
+    # Creates Dict for each Folder
+    for dTA in dirToAnnots:  # For each folder
         folderDict = {}
         folderDict['filePath'] = dTA
         folderDict['Annotated Images'] = len(os.listdir(dTA))
         allImages = allImages + len(os.listdir(dTA))
+
+        # Initializes counter for each key
         for q in allClasses:
             folderDict[q] = 0
 
+        # Initializes counter for key
         folderDict['Total Annotations'] = 0
-        for dA in dictArray:
-            # print dA
 
-            for dAA in dA:
-                if dAA in allClasses and dA['filePath'] == dTA:
-                    folderDict[dAA] = folderDict[dAA] + 1
+        for dA in dictArray:  # For each dict
+
+            for dAA in dA:  # For each entry in dict
+                if dAA in allClasses and dA['filePath'] == dTA:  # If current class and directory
+                    folderDict[dAA] = folderDict[dAA] + 1  # Increment counters
                     folderDict['Total Annotations'] = folderDict['Total Annotations'] + 1
         folderDicts.append(folderDict)
 
+    # creates dict with totals
     lastRowSummary = {}
-    columns = allClasses
     totalCounter = 0
-    imageCounter = 0
-    for c in columns:
+    for c in allClasses:
         counter = 0
         for d in folderDicts:
-            counter = counter + int(d[c])
-            imageCounter = imageCounter + 1
+            counter = counter + d[c]
         totalCounter = totalCounter + counter
         lastRowSummary[c] = counter
 
-    print imageCounter
     lastRowSummary['Annotated Images'] = allImages
     lastRowSummary['Total Annotations'] = totalCounter
     lastRowSummary['filePath'] = 'Total Annotations'
     folderDicts.append(lastRowSummary)
 
+    # Write to CSV file
     with open(CSVFILENAME + '.csv', 'w') as csvfile:
-        fieldnames = folderDicts[0].keys()
-        myorder = ['filePath']
+
+        # Organize headers in correct order
+        rowheaders = ['filePath']
         allClasses.sort()
         for c in allClasses:
-            myorder.append(c)
-        myorder.append('Total Annotations')
-        myorder.append('Annotated Images')
+            rowheaders.append(c)
+        rowheaders.append('Total Annotations')
+        rowheaders.append('Annotated Images')
 
-        filewriter = csv.DictWriter(csvfile, fieldnames=myorder)
+        filewriter = csv.DictWriter(csvfile, fieldnames=rowheaders)
         filewriter.writeheader()
 
-        for ww in folderDicts:
-            filewriter.writerow(ww)
+        for row in folderDicts:
+            filewriter.writerow(row)
