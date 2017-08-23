9a03296847ff669aca4dd71c6aea77332e979ecc
rtgoring <rtgoring@gmail.com>
2017-08-23 15:15:53 -0400

    Added progress indicator and multiple file support

--- fileMover.py
+++ fileMover.py
@@ -7,20 +7,24 @@ Moves objects from parent folder
 to location specified in text folder
 """
 
-parentFolder = '/home/goring/Documents/alex/darknet/devkit/skynet5'
+parent_folder = '/home/goring/Documents/alex/darknet/devkit/skynet2000'
 text_files = ['train_annot.txt', 'train_image.txt', 'valid_annot.txt', 'valid_image.txt']
 
-
-for textFileName in text_files:
-    destinationFolder = os.path.join(parentFolder, textFileName.split('.')[0])
+counter = 1
+for f in text_files:  # For each file
+    destination_folder = os.path.join(parent_folder, f.split('.')[0])
 
     try:
-        os.mkdir(destinationFolder)
+        os.mkdir(destination_folder)
     except:
         pass  # Already there
-    file_opener = open(os.path.join(parentFolder, textFileName), 'r')
 
-    for line in file_opener:
-        print line
-        line = line.split('\n')[0]
-        shutil.copy(line, os.path.join(parentFolder, destinationFolder))
+    file_reader = open(os.path.join(parent_folder, f), 'r')
+
+    for line in file_reader:  # For each line
+        line = line.split('\n')[0]  # Read line
+        shutil.copy(line, os.path.join(parent_folder, destination_folder))  # Copy from location to new folder
+    print counter
+    counter = counter + 1
+
+print "Done!"
